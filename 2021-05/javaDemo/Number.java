547d25b9204e71d1d0810b82bce04e4d820b7717
bmxklyzj <948228952@qq.com>
2021-06-19 23:22:27 +0800

    update learn java

---
+++ 2021-05/javaDemo/Number.java
@@ -0,0 +1,15 @@
+public class Number {
+    public static void main(String[] args) {
+        System.out.println(5.0/0);
+        System.out.println(5.0/-0);
+        System.out.println(-5.0/0);
+        System.out.println(0.0/0);
+
+        char zero = '0';
+        char A = 'A';
+        char a = 'a';
+        System.out.println((int) zero);
+        System.out.println((int) A);
+        System.out.println((int) a);
+    }
+}

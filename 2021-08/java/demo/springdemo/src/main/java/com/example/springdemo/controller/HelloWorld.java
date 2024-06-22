017ac77e09d18edfb6d312d0a029e1a0275ce69c
bmxklyzj <948228952@qq.com>
2024-06-23 00:51:41 +0800

    java

--- 2021-08/java/demo/springdemo/src/main/java/com/example/springdemo/controller/HelloWorld.java
+++ 2021-08/java/demo/springdemo/src/main/java/com/example/springdemo/controller/HelloWorld.java
@@ -1,14 +1,12 @@
 package com.example.springdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorld {
     @RequestMapping("/hello")
-    @ResponseBody
     public String hello() {
-        return "hello spring";
+        return "hello spring2";
     }
 }
